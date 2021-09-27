4fb773c07db733a870da8712eff17eabf2f26d59
Deepak Verma <0822deepakverma@gmail.com>
2021-09-27 20:44:17 +0530

    code updated

--- src/notes.js
+++ src/notes.js
@@ -1,44 +1,48 @@
-const notes=[
-{
-  key:1,
-  title:"twitter",
-  content:"Twitter is a good twitter is it so"
-},
-{
-  key:2,
-  title:"facebook",
-  content:"facebook is a good facebook"
-},
-{
-  key:3,
-  title:"Programmer",
-  content:"My name is deepak verma 2011 @ live.com"
-},
-{
-  key:4,
-  title:"Programmer",
-  content:"My name is deepak verma 2011 @ live.com"
-},
-{
-  key:5,
-  title:"Programmer",
-  content:"My name is deepak verma 2011 @ live.com"
-},
-{
-  key:6,
-  title:"Programmer",
-  content:"My name is deepak verma 2011 @ live.com"
-},
-{
-  key:7,
-  title:"Programmer",
-  content:"My name is deepak verma 2011 @ live.com"
-},
-{
-  key:8,
-  title:"Programmer",
-  content:"My name is deepak verma 2011 @ live.com"
-}
+const notes = [{
+    key: 1,
+    title: "twitter",
+    content: "Twitter is a good twitter is it so"
+  },
+  {
+    key: 2,
+    title: "facebook",
+    content: "facebook is a good facebook"
+  },
+  {
+    key: 3,
+    title: "Programmer",
+    content: "My name is deepak verma 2011 @ live.com"
+  },
+  {
+    key: 4,
+    title: "Programmer",
+    content: "My name is deepak verma 2011 @ live.com"
+  },
+  {
+    key: 5,
+    title: "Programmer",
+    content: "My name is deepak verma 2011 @ live.com"
+  },
+  {
+    key: 6,
+    title: "Programmer",
+    content: "My name is deepak verma 2011 @ live.com"
+  },
+  {
+    key: 7,
+    title: "Programmer",
+    content: "My name is deepak verma 2011 @ live.com"
+  },
+  {
+    key: 8,
+    title: "Programmer",
+    content: "My name is deepak verma 2011 @ live.com"
+  },
+  {
+    key: 9,
+    title: "Programmers",
+    content: "My name is deepakverma2011@l"
+  }
 ]
 
 export default notes;
